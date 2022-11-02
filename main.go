250dbc34a7ae99a1e0dfaf5f7ec62bcfdb505a2b
Sanzhar Umarov <sanzharius1999@gmail.com>
2022-11-02 22:49:26 +0600

    Add Task 2.1

--- main.go
+++ main.go
@@ -2,22 +2,68 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"os"
+)
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("/about"),
+		tgbotapi.NewKeyboardButton("/links"),
+	),
 )
 
 func main() {
-	ConvertTime()
-}
-
-func ConvertTime() string {
-	l1 := "03:04:05PM"
-	l2 := "15:04:05"
-	t, err := time.Parse(l1, "07:05:45PM")
+	cfg, err := Init()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+	}
+	bot, err := tgbotapi.NewBotAPI(os.Getenv(cfg.My_token))
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+		switch update.Message.Text {
+		case "/start", "/help":
+			msg.ReplyMarkup = numericKeyboard
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+		log.Print("starting server...")
+
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080"
+		}
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+		/*KeyboardButtons := tgbotapi.NewKeyboardButton("")
+
+		switch KeyboardButtons {
+		case tgbotapi.NewKeyboardButton("/about"):
+			fmt.Println("Hi, my name is Sanzhar, I study Go")
+		case tgbotapi.NewKeyboardButton("/links"):
+			fmt.Println("https://github.com/sanzharius, https://www.linkedin.com/in/sanzhar-umarov-713818252/")
+		}*/
 
 	}
-	fmt.Println(t.Format(l2))
-	return t.Format(l2)
-
 }
